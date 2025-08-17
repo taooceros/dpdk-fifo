216354f56c6096f496765bb6c8ad3187eca54f21
Hongtao <hongtao_zhang@outlook.com>
2025-08-17 23:13:09 +0000

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,20 @@
 add_rules("mode.debug", "mode.release")
 
+
 add_includedirs("include")
-add_requires("libdpdk", {system = true})
+
+add_requires("pkgconfig::libdpdk", {system = true})
+
 set_languages("c++23")
 
 
 target("client")
     set_kind("binary")
     add_files("client-src/*.cpp", "common/*.cpp")
-    add_packages("libdpdk")
+    add_packages("pkgconfig::libdpdk")
 
 
 target("server")
     set_kind("binary")
     add_files("server-src/*.cpp", "common/*.cpp")
-    add_packages("libdpdk")
+    add_packages("pkgconfig::libdpdk")
