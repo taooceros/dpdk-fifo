3001f2433474cd52775dbf3e808373690d915130
Hongtao <hongtao_zhang@outlook.com>
2025-08-18 00:13:16 +0000

    use custom rule to link to dpdk

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,28 @@
+
+rule("dpdk_linker_flags")
+    on_load(function (target)
+        -- Call external tool to generate flags
+        local output = os.iorunv("pkg-config", {"--static", "--libs", "libdpdk"})
+        if output then
+            target:add("ldflags", output:trim())
+        end
+    end)
+    on_build(function (target)
+        local includedir = os.iorunv("pkg-config", {"--cflags-only-I", "libdpdk"})
+        if includedir then
+            target:add("includedirs", includedir:trim())
+        end
+    end)
+rule_end()
+
 add_rules("mode.debug", "mode.release")
 
 
 add_includedirs("include")
 
-add_requires("pkgconfig::libdpdk", {system = true})
+add_requires("pkgconfig::libdpdk", {system = true, configs = {
+    shared = false,
+}})
 
 set_languages("c++23")
 
@@ -11,10 +30,10 @@ set_languages("c++23")
 target("client")
     set_kind("binary")
     add_files("client-src/*.cpp", "common/*.cpp")
-    add_packages("pkgconfig::libdpdk")
+    add_rules("dpdk_linker_flags")
 
 
 target("server")
     set_kind("binary")
     add_files("server-src/*.cpp", "common/*.cpp")
-    add_packages("pkgconfig::libdpdk")
+    add_rules("dpdk_linker_flags")
