f191841e5a65333d3701218fd5c6097e8a4ecba9
Hongtao <hongtao_zhang@outlook.com>
2025-08-18 00:20:21 +0000

    use cflags instead of include dir

--- xmake.lua
+++ xmake.lua
@@ -10,7 +10,7 @@ rule("dpdk_linker_flags")
     on_build(function (target)
         local includedir = os.iorunv("pkg-config", {"--cflags-only-I", "libdpdk"})
         if includedir then
-            target:add("includedirs", includedir:trim())
+            target:add("cflags", includedir:trim())
         end
     end)
 rule_end()
